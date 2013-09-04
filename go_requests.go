e725ed30cdc4e40cd0a551a01bc13be4ce05d4b0
Goncalo Pereira <goncalo@goncalo-linux>
2013-09-04 15:27:32 +0100

    renaming files, adding tests

--- go_requests.go
+++ go_requests.go
@@ -1,4 +1,4 @@
-package main
+package go_requests
 
 import (
   "fmt"
---
+++ go_requests_test.go
@@ -0,0 +1,18 @@
+package go_requests
+
+import "testing"
+
+func TestCreateUrl(t *testing.T) {
+  poolId, trackId, formatId := 1, 2, 3
+  u := "http://mediapool1.nix.sys.7d/track/2/format/3"
+
+  result, err := createUrl(poolId, trackId, formatId)
+  
+  if result.String() != u {
+    t.Errorf("expected %v and got %v", u, result)
+  }
+  
+  if err != nil {
+    t.Error("Unexpected error returned")
+  }
+}
